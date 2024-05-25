c3618ce8fa2036a1b82d4a280b26a568d46b3e97
FxIvan <almendra21014@gmail.com>
2024-05-25 19:18:29 -0300

    creando puerto para escuchar peticiones

--- microservice/cmd/app/main.go
+++ microservice/cmd/app/main.go
@@ -9,9 +9,19 @@ import (
 	"time"
 )
 
+type ConnectDB struct {
+	Engine   *string
+	Host     *string
+	Port     *int
+	User     *string
+	Password *string
+	DBName   *string
+}
+
 type application struct {
 	error_log *log.Logger
 	info_log  *log.Logger
+	connDB    *ConnectDB
 }
 
 func main() {
@@ -20,11 +30,27 @@ func main() {
 	//go run . -srvPort 8000 -srvAddr localhost
 	server_addr := flag.String("srvAddr", "", "HTTP server network address")
 	server_port := flag.Int("srvPort", 4000, "HTTP server network port")
+	input_engine := flag.String("enginedb", "", "Engine DB")
+	input_host := flag.String("hostdb", "localhost", "Host DB")
+	input_port := flag.Int("portdb", 3360, "Port DB")
+	input_user := flag.String("userdb", "", "User DB")
+	input_password := flag.String("passwordb", "", "Password DB")
+	input_dbname := flag.String("dbname", "", "Name DB")
 	flag.Parse()
+
+	conn_obj := &ConnectDB{
+		Engine:   input_engine,
+		Host:     input_host,
+		Port:     input_port,
+		User:     input_user,
+		Password: input_password,
+		DBName:   input_dbname,
+	}
 
 	app := &application{
 		error_log: error_log,
 		info_log:  info_log,
+		connDB:    conn_obj,
 	}
 
 	serverURI := fmt.Sprintf("%s:%d", *server_addr, *server_port)
