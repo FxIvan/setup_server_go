27b8234137313e4c6ed128194d259c5b55ee1b7d
FxIvan <almendra21014@gmail.com>
2024-05-30 20:53:45 -0300

    agregando createUser en mysql

--- microservice/internal/adapter/storage/mysql/mysql.go
+++ microservice/internal/adapter/storage/mysql/mysql.go
@@ -15,7 +15,7 @@ type MySQL struct {
 }
 
 func New(config *configuration.Configuration) (*MySQL, error) {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", config.User, config.Password, config.Host, config.Port, config.DBName)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", config.User, config.Password, config.Host, config.Port, "users")
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		return nil, fmt.Errorf("could not connect to mysql: %w", err)
@@ -29,5 +29,32 @@ func New(config *configuration.Configuration) (*MySQL, error) {
 }
 
 func (m *MySQL) CreateUserStorage(userModel *domain.User, collectionName string) (string, error) {
-	return "", nil
+	// Crear la tabla si no existe
+	createTableQuery := `
+	    CREATE TABLE IF NOT EXISTS users (
+	        product_id int primary key auto_increment,
+	        email text,
+	        name text,
+	        password text,
+	        role text,
+	        created_at datetime default CURRENT_TIMESTAMP,
+	        updated_at datetime default CURRENT_TIMESTAMP
+	    )
+	`
+	_, err := m.DB.Exec(createTableQuery)
+	if err != nil {
+		return "", fmt.Errorf("could not create table: %v", err)
+	}
+
+	// Insertar datos del usuario en la tabla
+	insertUserQuery := `
+	    INSERT INTO users (email, name, password, role) VALUES (?, ?, ?, ?)
+	`
+	_, err = m.DB.Exec(insertUserQuery, userModel.Email, userModel.Name, userModel.Password, userModel.Role)
+	if err != nil {
+		return "", fmt.Errorf("could not insert user: %v", err)
+	}
+
+	fmt.Printf("Function Save | data to MySQL %s", userModel)
+	return "User Created", nil
 }
