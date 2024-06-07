30d0141dd6134f8f8323bcac57fcb9fafb28f662
FxIvan <almendra21014@gmail.com>
2024-06-07 10:03:41 -0300

    creando script para deploy de docker compose

--- script/init.sh
+++ script/init.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-echo microservice:$1 >> nameContainer.txt
+# Cambia el directorio a /home/almendra/set_up_server/microservice
 cd /home/almendra/set_up_server/microservice
-docker build -t almendraivan/microservice:v$1 .
-docker run -p 4000:4000 --name microservice-v$1 --network service-mongodb almendraivan/microservice:v$1
+
+# Toma el primer parámetro como la versión
+version=$1
+
+# Concatenar el nombre del contenedor con la versión
+name_container="NAME_CONTAINER_ENV=microservice-v$version"
+
+# Escribe el nombre del contenedor en el archivo nameContainer.txt
+cd /home/almendra/script
+echo "$name_container" >> .env
+cd /home/almendra/set_up_server/microservice
+docker compose down
+docker compose rm
+docker rmi $(docker images -q)
+
+# Construye la imagen del microservicio
+docker compose build --no-cache > log-docker-compose-build.log
+#Sacar los log dentro del log-dockerCompose.log
+docker compose up --build -d > log-docker-compose-up.log
